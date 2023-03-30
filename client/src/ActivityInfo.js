ce3964f1ccce0e1f3e01aa4149a9c3f26aac603e
Steven McVey <smcvey1210@gmail.com>
2023-03-30 15:00:37 -0700

    add descriptions to ActivityInfo

--- client/src/ActivityInfo.js
+++ client/src/ActivityInfo.js
@@ -2,9 +2,24 @@ import React from "react";
 
 function ActivityInfo({activity}){
 
+  const descriptions = {
+    basicSkillsLittles: "basicSkillsLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    trackLittles: "trackLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    soccerLittles: "soccerLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    baseballLittles: "baseballLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    advancedSkillsBigs: "advancedSkillsBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    trackBigs: "trackBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    soccerBigs: "soccerBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    baseballBigs: "baseballBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    scienceLittles: "scienceLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    scienceBigs: "scienceBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    artLittles: "artLittles: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+    artBigs: "artBigs: Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"
+  }
+
   return(
     <div>
-      <p>{activity}</p>
+      <p>{descriptions[activity]}</p>
     </div>
   )
 }
