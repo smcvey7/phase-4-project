dab880d8bd73e4f6983c4361c25f5f2c15144154
Steven McVey <smcvey1210@gmail.com>
2023-04-05 12:16:00 -0700

    update styling for Home

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,21 +1,21 @@
 
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Home(){
+  const navigate = useNavigate()
+
   return(
     <div id="home" className="flexContainer vertical">
       <h2>CAMPS & CLASSES</h2>
       <div className="flexContainer horizontal spaceAround">
-        <div>
+        <div className="activityBox">
           <h2>SPORT CAMPS</h2>
-          <img src="./img/soccer_5_7.jpeg" alt="sports camps" width="400px" height="230px" /><br/>
-          <NavLink to="/sports">Learn more</NavLink>
+          <img src="./img/soccer_5_7.jpeg" alt="sports camps" onClick={()=>navigate('/sports')} /><br/>
         </div>
-        <div>
+        <div className="activityBox">
           <h2>DISCOVERY CAMPS</h2>
-          <img src="./img/science_5_7_3.jpeg" alt="discovery camps" width="400px" height="230px" /><br/>
-          <NavLink to="/discovery">Learn more</NavLink>
+          <img src="./img/science_5_7_3.jpeg" alt="discovery camps" onClick={()=>navigate('/discovery')} /><br/>
         </div>
       </div>
     </div>
