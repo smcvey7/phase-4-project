ff110d552ef4cb02082cf9a130d894fc4c2cf00a
Steven McVey <smcvey1210@gmail.com>
2023-04-20 13:59:25 -0700

    add filter to CamperRegistrations

--- client/src/CamperRegistrations.js
+++ client/src/CamperRegistrations.js
@@ -7,8 +7,8 @@ function CamperRegistrations({camper}){
   const [activities, setActivities] = useState(null)
   const [filteredActivities, setFilteredActivities] = useState(null)
   const [currentSelection, setCurrentSelection] = useState({
-    age: "all",
-    time: "all"
+    age_group: "all",
+    dates: "all"
   })
 
   useEffect(()=>{
@@ -20,9 +20,29 @@ function CamperRegistrations({camper}){
       .then((data)=>{
         setActivities(data)
         setFilteredActivities(data)
+        setCurrentSelection(({
+          age_group: "all",
+          dates: "all"
+        }))
       })
     }
   }, [navigate, camper])
+
+  useEffect(()=>{
+    if (!activities) return null
+    const updatedList = activities.filter((activity)=>{
+      if (currentSelection.age_group === "all" && currentSelection.dates === "all"){
+        return true
+      }else if (currentSelection.age_group === "all"){
+        return activity.dates === currentSelection.dates
+      }else if (currentSelection.dates === "all"){
+        return currentSelection.age_group === activity.age_group
+      }else{
+        return currentSelection.age_group === activity.age_group && currentSelection.dates === activity.dates
+      }
+    })
+    setFilteredActivities(updatedList)
+  }, [currentSelection, activities, navigate])
 
 function handleChange(e){
   setCurrentSelection({
@@ -36,14 +56,14 @@ function handleChange(e){
       <p>Registration List</p>
       <form>
         Age:
-        <select name="age" value={currentSelection} onChange={handleChange}>
+        <select name="age_group" value={currentSelection.age_group} onChange={handleChange}>
           <option value="all">All ages</option>
           <option value="littles">Littles</option>
           <option value="bigs">Bigs</option>
         </select>
         Dates:
-        <select name="time" value={currentSelection} onChange={handleChange}>
-          <option value="all">All times</option>
+        <select name="dates" value={currentSelection.dates} onChange={handleChange}>
+          <option value="all">All dates</option>
           <option value="time1">6/5-16</option>
           <option value="time2">6/19-30</option>
           <option value="time3">7/3-14</option>
